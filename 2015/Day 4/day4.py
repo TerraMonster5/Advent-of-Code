0fae197419cf593e5f10d97a5378cc5efd28fe98
TerraMonster5 <william.dargan@gmail.com>
2023-01-07 13:12:42 +0000

    Added day 4

---
+++ 2015/Day 4/day4.py
@@ -0,0 +1,10 @@
+import hashlib
+code = "bgvyzdsv"
+def checkHash(string: str) -> int:
+    num = -1
+    hexHash = hashlib.md5(code.encode()).hexdigest()
+    while not hexHash.startswith(string):
+        num += 1
+        hexHash = hashlib.md5(str(code+str(num)).encode()).hexdigest()
+    return num
+print(checkHash("00000"),checkHash("000000"))
