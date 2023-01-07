c88be4c929c3f297e218242f73eae92b430cd09e
TerraMonster5 <william.dargan@gmail.com>
2023-01-07 13:56:49 +0000

    Added day 5 part 1

--- 2015/Day 4/day4.py
+++ 2015/Day 4/day4.py
@@ -7,4 +7,4 @@ def checkHash(string: str) -> int:
         num += 1
         hexHash = hashlib.md5(str(code+str(num)).encode()).hexdigest()
     return num
-print(checkHash("00000"),checkHash("000000"))
+print(checkHash("00000"), checkHash("000000"))
--- 2015/Day 5/day5part1.py
+++ 2015/Day 5/day5part1.py
@@ -1,1 +1,12 @@
 with open("input.txt","r") as file: strings = [line.strip() for line in file]
+def has3Vowels(checkVal) -> bool:
+    num = 0
+    for char in checkVal:
+        if char in ("a","e","i","o","u"): num += 1
+    if num > 2: return True
+    else: return False
+nice = 0
+for string in strings:
+    forbidden = sum((string.count("ab"),string.count("cd"),string.count("pq"),string.count("xy")))
+    if forbidden == 0 and any(a==b for a,b in zip(string,string[1:])) and has3Vowels(string): nice += 1
+print(nice)
