002db949b3764c345d1622923403fae6ae5fdffa
TerraMonster5 <william.dargan@gmail.com>
2023-01-09 16:28:23 +0000

    Added to day 9

--- 2015/Day 9/day9part1.py
+++ 2015/Day 9/day9part1.py
@@ -1,4 +1,18 @@
 from structs import Graph
 
-with open("input.txt", "r") as file: paths = [line.strip().split() for line in file]
-places = [path[0] for path in paths]
+with open("input.txt", "r") as file: lines = [line.strip().split() for line in file]
+refTable = tuple(dict.fromkeys(path[0] for path in lines))
+paths: dict = {}
+for line in lines:
+    if line[2] == "Arbre":
+        continue
+    elif line[0] in paths.keys():
+        paths[line[0]][refTable.index(line[2])] = int(line[-1])
+    else:
+        paths[line[0]] = {}
+        paths[line[0]][refTable.index(line[2])] = int(line[-1])
+
+world = Graph()
+
+for path in paths.values():
+    world.vertices.append(world.Vertex(path))
