d10b94ec76b3408af2800127a75722eb3db685a2
TerraMonster5 <william.dargan@gmail.com>
2023-01-09 12:26:08 +0000

    Finished day 6 part 1 and added day 6 part 2

--- 2015/Day 6/day6part2.py
+++ 2015/Day 6/day6part2.py
@@ -7,7 +7,7 @@ def formatRaw(instruction: list) -> list:
     return instruction
 
 raw = list(map(formatRaw, instructions))
-lights = [[False for x in range(1000)] for x in range(1000)]
+lights = [[0 for x in range(1000)] for x in range(1000)]
 
 def lightControl(rawInstruction: list) -> None:
     global lights
@@ -15,17 +15,18 @@ def lightControl(rawInstruction: list) -> None:
         for y in range(rawInstruction[1][1], rawInstruction[2][1]+1):
             match rawInstruction[0]:
                 case "toggle":
-                    lights[x][y] = not lights[x][y]
+                    lights[x][y] += 2
                 case "on":
-                    lights[x][y] = True
+                    lights[x][y] += 1
                 case "off":
-                    lights[x][y] = False
+                    if lights[x][y] > 0:
+                        lights[x][y] -= 1
 
 for command in raw:
     lightControl(command)
 
 total = 0
 for row in lights:
-    total += row.count(True)
+    total += sum(row)
 
 print(total)
