efb4f1a7f2e9881eb8d364fdddeb52051f7e09f4
TerraMonster5 <william.dargan@gmail.com>
2023-01-21 00:12:15 +0000

    Commented day 10

--- 2015/Day 10/day10.py
+++ 2015/Day 10/day10.py
@@ -8,11 +8,13 @@ def runLength(text: str) -> str:
         newCode += str(len(group)) + group[0]
     return newCode
 
+# Part 1
 for x in range(40):
     code = runLength(code)
 
 print(len(code))
 
+# Part 2
 for x in range(10):
     code = runLength(code)
 
