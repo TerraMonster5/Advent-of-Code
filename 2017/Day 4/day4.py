51578b9857bf17101f4a003c46dc7c961fff02fd
TerraMonster5 <william.dargan@gmail.com>
2023-01-21 00:09:59 +0000

    Finished day 4

--- 2017/Day 4/day4.py
+++ 2017/Day 4/day4.py
@@ -1,1 +1,18 @@
-with open("input.txt","r") as file: pass
+from itertools import permutations
+
+with open("input.txt","r") as file: passphrases = [line.strip().split() for line in file]
+
+# Part 1
+print(len([phrase for phrase in passphrases if len(set(phrase)) == len(phrase)]))
+
+# Part 2
+valid = 0
+
+for phrase in passphrases:
+    anagrams = list(map(lambda x: sorted(x[0]) == sorted(x[1]), permutations(phrase, 2)))
+    if anagrams.count(True) > 0:
+        continue
+    if len(set(phrase)) == len(phrase):
+        valid += 1
+
+print(valid)
