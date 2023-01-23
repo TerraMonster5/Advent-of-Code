baf33c9944744dd9a08477c61f2b21cfab9177e1
TerraMonster5 <93338246+TerraMonster5@users.noreply.github.com>
2023-01-23 15:44:46 +0000

    Edited day 3

--- 2017/Day 3/day3.py
+++ 2017/Day 3/day3.py
@@ -1,5 +1,18 @@
 from math import sqrt
 
+X = 0
+Y = 1
+
 val = 289326
 
-layer = int(sqrt(val))
+oddRoot = int(sqrt(val))
+
+oddSquare = oddRoot**2
+
+layer = oddRoot//2
+
+coords = [layer]*2
+
+coords[X] += 1
+
+val -= (oddSquare + 1)
