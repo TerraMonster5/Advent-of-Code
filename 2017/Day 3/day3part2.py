be293e4766fcd4f4f9a7352a0b27f0fa6073b107
TerraMonster5 <william.dargan@gmail.com>
2023-01-25 22:00:14 +0000

    Deleted day 3 part 2

--- 2017/Day 3/day3part2.py
+++
@@ -1,9 +0,0 @@
-val = 289326
-
-class Spiral:
-    def __init__(self):
-        self._data = {(0, 0): 1, (1, 0): 1}
-    
-    def build(self, checkVal):
-        while max(self._data.values()) <= checkVal:
-            pass
