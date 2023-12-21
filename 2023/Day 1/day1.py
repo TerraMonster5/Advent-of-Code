6b0160dd9aba5dd488786636cba60a3e5fd1dc05
Will <93338246+TerraMonster5@users.noreply.github.com>
2023-12-21 10:33:45 +0000

    Updated day 1

--- 2023/Day 1/day1.py
+++ 2023/Day 1/day1.py
@@ -5,16 +5,4 @@ nums = ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9")
 with open("input.txt", "r") as file: lines = [line.rstrip() for line in file]
 indices = [[None if line.find(x) == -1 else line.find(x) for x in numstrings] for line in lines]
 first = [lines[count].replace(numstrings[x.index(min(filter(lambda a: a != None, x)))], nums[x.index(min(filter(lambda a: a != None, x)))], 1) for count, x in enumerate(indices) if any(x)]
-last = [lines[count].replace(numstrings[x.index(max(filter(lambda a: a != None, x)))], nums[x.index(max(filter(lambda a: a != None, x)))], 1) for count, x in enumerate(indices) if any(x)]
-for i in range(len(lines)):
-    pass
-
-
-strValue = "This is the last rain of season and Jack is here."
-# Substring that need to be replaced
-strToReplace   = 'is'
-# Replacement substring
-replacementStr = 'XX'
-# Replace last occurrences of substring 'is' in string with 'XX'
-strValue = replacementStr.join(strValue.rsplit(strToReplace, 1))
-print(strValue)
+last = [nums[x.index(max(filter(lambda a: a != None, x)))].join(lines[count].rsplit(numstrings[x.index(max(filter(lambda a: a != None, x)))], 1)) for count, x in enumerate(indices) if any(x)]
