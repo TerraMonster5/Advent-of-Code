86b090e0f2b411e68a0ab5a362e4e9026ce40890
TerraMonster5 <william.dargan@gmail.com>
2024-01-01 20:33:52 +0000

    Updated day 1 part 2

--- 2023/Day 1/day1.py
+++ 2023/Day 1/day1.py
@@ -1,8 +1,26 @@
+# Part 1
 with open("input.txt", "r") as file: print(sum([int([x for x in line.rstrip() if x.isdigit()][0]+[x for x in line.rstrip() if x.isdigit()][-1]) for line in file]))
 
-numstrings = ("zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
-nums = ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9")
+# Part 2
+numstrings = ("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+nums = ("1", "2", "3", "4", "5", "6", "7", "8", "9")
 with open("input.txt", "r") as file: lines = [line.rstrip() for line in file]
 indices = [[None if line.find(x) == -1 else line.find(x) for x in numstrings] for line in lines]
-first = [lines[count].replace(numstrings[x.index(min(filter(lambda a: a != None, x)))], nums[x.index(min(filter(lambda a: a != None, x)))], 1) for count, x in enumerate(indices) if any(x)]
-last = [nums[x.index(max(filter(lambda a: a != None, x)))].join(lines[count].rsplit(numstrings[x.index(max(filter(lambda a: a != None, x)))], 1)) for count, x in enumerate(indices) if any(x)]
+print(indices)
+print([list(filter(lambda a: a != None, x)) for x in indices])
+first = [lines[count].replace(numstrings[x.index(min(list(filter(lambda a: a != None, x))))], nums[x.index(min(list(filter(lambda a: a != None, x))))], 1) if any(x) else lines[count] for count, x in enumerate(indices)]
+last = [nums[x.index(max(list(filter(lambda a: a != None, x))))].join(lines[count].rsplit(numstrings[x.index(max(list(filter(lambda a: a != None, x))))], 1)) if any(x) else lines[count] for count, x in enumerate(indices)]
+news = []
+for x in range(len(lines)):
+    temp = ""
+    for count, char in enumerate(first[x]):
+        if char.isdigit():
+            temp += char
+            break
+    for count, char in enumerate(last[x][::-1]):
+        if char.isdigit():
+            temp += char
+            break
+    news.append(int(temp))
+print(news)
+print(sum(news))
