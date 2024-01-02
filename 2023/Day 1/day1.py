af87fa1e31a69c65a524eae318e4520cbdd163b9
TerraMonster5 <william.dargan@gmail.com>
2024-01-02 17:36:48 +0000

    Updated gitignore and fixed day 1 part 2

--- 2023/Day 1/day1.py
+++ 2023/Day 1/day1.py
@@ -2,12 +2,14 @@
 with open("input.txt", "r") as file: print(sum([int([x for x in line.rstrip() if x.isdigit()][0]+[x for x in line.rstrip() if x.isdigit()][-1]) for line in file]))
 
 # Part 2
+import re
 numstrings = ("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
 nums = ("1", "2", "3", "4", "5", "6", "7", "8", "9")
 with open("input.txt", "r") as file: lines = [line.rstrip() for line in file]
-indices = [[None if line.find(x) == -1 else line.find(x) for x in numstrings] for line in lines]
-first = [lines[count].replace(numstrings[x.index(min(list(filter(lambda a: a != None, x))))], nums[x.index(min(list(filter(lambda a: a != None, x))))], 1) if any(x) else lines[count] for count, x in enumerate(indices)]
-last = [nums[x.index(max(list(filter(lambda a: a != None, x))))].join(lines[count].rsplit(numstrings[x.index(max(list(filter(lambda a: a != None, x))))], 1)) if any(x) else lines[count] for count, x in enumerate(indices)]
+firstindices = [[None if line.find(x) == -1 else line.find(x) for x in numstrings] for line in lines]
+lastindices = [[None if line.find(x) == -1 else line.rfind(x) for x in numstrings] for line in lines]
+first = [lines[count].replace(numstrings[x.index(min(list(filter(lambda a: a != None, x))))], nums[x.index(min(list(filter(lambda a: a != None, x))))], 1) if any(map(lambda a: isinstance(a, int), x)) else lines[count] for count, x in enumerate(firstindices)]
+last = [nums[x.index(max(list(filter(lambda a: a != None, x))))].join(lines[count].rsplit(numstrings[x.index(max(list(filter(lambda a: a != None, x))))], 1)) if any(map(lambda a: isinstance(a, int), x)) else lines[count] for count, x in enumerate(lastindices)]
 news = []
 for x in range(len(lines)):
     temp = ""
@@ -20,5 +22,4 @@ for x in range(len(lines)):
             temp += char
             break
     news.append(int(temp))
-print(news)
 print(sum(news))
