2c3f735571d0dd9568fe73277bab5721f9a2b4bd
TerraMonster5 <william.dargan@gmail.com>
2024-01-05 14:10:20 +0000

    Added day 2 part 1

---
+++ 2023/Day 2/day2part1.py
@@ -0,0 +1,12 @@
+with open("input.txt", "r") as file: lines = [[{hand.split()[1]: int(hand.split()[0]) for hand in game.split(", ")} for game in line.strip().split(": ")[1].split("; ")] for line in file]
+total = 0
+for count, game in enumerate(lines, 1):
+    temp = {"red": 0, "green": 0, "blue": 0}
+    for hand in game:
+        for key, value in hand.items():
+            if value > temp[key]:
+                temp[key] = value
+    print(count, temp, [not {"red": 12, "green": 13, "blue": 14}[key] < temp[key] for key in temp.keys()])
+    if all(not {"red": 12, "green": 13, "blue": 14}[key] < temp[key] for key in temp.keys()):
+        total += count
+print(total)
