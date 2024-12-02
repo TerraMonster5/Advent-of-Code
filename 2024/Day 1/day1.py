edfdff3b5b5e656f66284b8c962a2f70b50abcbb
TerraMonster5 <william.dargan@gmail.com>
2024-12-02 03:26:35 +0000

    Added 2024 Day 1 Part 2

--- 2024/Day 1/day1.py
+++ 2024/Day 1/day1.py
@@ -1,1 +1,1 @@
-with open("input.txt", "r") as file: print(sum(map(lambda y: abs(y[0]-y[1]), zip(*map(lambda x: sorted(list(x)), zip(*[list(map(int, line.strip().split())) for line in file]))))))
+with open("input.txt", "r") as file: left, right = map(lambda x: sorted(list(x)), zip(*[list(map(int, line.strip().split())) for line in file])); print(sum(map(lambda y: abs(y[0]-y[1]), zip(left, right)))); print(sum([num*right.count(num) for num in left]))
